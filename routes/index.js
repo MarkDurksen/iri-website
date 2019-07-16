f82da5a74178a9dfa192f7cc15ebc8aef6528c5f
MarkDurksen <markdurksen@gmail.com>
2019-07-16 09:36:12 -0400

    Create About, Projects and Contact routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', {title: 'About'});
+});
+
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', {title: 'Projects'});
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', {title: 'Contact'});
 });
 
 module.exports = router;
