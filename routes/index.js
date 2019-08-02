fc88d9b1e4b4fd85904b8c815a6d94d1fc63652f
MarkDurksen <markdurksen@gmail.com>
2019-08-02 15:48:41 -0400

    Add contact form functionality

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const nodemailer = require("nodemailer");
+require('dotenv/config');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,7 +21,66 @@ router.get('/projects', function(req, res, next) {
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('contact', {title: 'Contact'});
+  res.render('contact', {
+    title: 'Contact',
+    msg: ''
+  });
+});
+
+/* POST contact page. */
+router.post('/send', (req, res) => {
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>
+        <li>Name: ${req.body.name}</li>
+        <li>Name: ${req.body.email}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+
+  // async..await is not allowed in global scope, must use a wrapper
+  async function main(){
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+          user: process.env.SENDER_USER, // generated ethereal user
+          pass: process.env.SENDER_PASS // generated ethereal password
+      },
+      tls:{
+         rejectUnauthorized:false
+      }
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+        from: '"Nodemailer Contact" <iriwebsitecontactform@gmail.com>', // sender address
+        to: process.env.RECIPIENT_USER, // list of receivers
+        subject: "Node Contact Request", // Subject line
+        text: "Hello world?", // plain text body
+        html: output // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+    res.render('contact', {
+        title: 'Contact',
+        msg: 'Message has been sent.'
+    });
+  }
+
+  main().catch(console.error);
+
 });
 
 module.exports = router;
